da803362412eac40f2230417ab8f6adfa15cc9db
Mike Spertus <mike@spertus.com>
2023-05-31 09:06:04 -0500

    Add support for async invocation

--- include/awslabs/enhanced/lambda_client.h
+++ include/awslabs/enhanced/lambda_client.h
@@ -76,7 +76,8 @@ template<typename R, typename ...Args>
 struct Lambda<R(Args...)> {
   Lambda(EnhancedLambdaClient &client, std::string name)
       : client(client), name(name) {}
-  R handleSuccessfulInvocation(Aws::Lambda::Model::InvokeResult &result) {
+
+  R handleSuccessfulInvocation(Aws::Lambda::Model::InvokeResult const &result) {
     Aws::IOStream &payload = result.GetPayload();
     // h/t https://stackoverflow.com/questions/3203452/how-to-read-entire-stream-into-a-stdstring
     Aws::String ret(std::istreambuf_iterator<char>(payload), {});
@@ -93,6 +94,13 @@ struct Lambda<R(Args...)> {
     if(ec) 
       throw std::runtime_error("Deserialization error code: " + ec.message());
     return resultHolder.result;
+  }
+
+  expns::expected <R, std::string> outcomeToExpected(Aws::Lambda::Model::InvokeOutcome const &outcome) {
+    if(outcome.IsSuccess()) 
+      return handleSuccessfulInvocation(outcome.GetResult());
+    else
+      return expns::unexpected(outcome.GetError().GetMessage());
   }
 
   R operator()(Args... args) {
@@ -118,6 +126,34 @@ struct Lambda<R(Args...)> {
     Aws::Lambda::LambdaError e = outcome.GetError();
     throw std::runtime_error(e.GetMessage());
   }
+
+  template<typename Callable>
+  void invoke_async(Callable c, Args... args) {
+    Aws::Lambda::Model::InvokeRequest invokeRequest;
+    invokeRequest.SetFunctionName(name);
+    invokeRequest.SetInvocationType(Aws::Lambda::Model::InvocationType::RequestResponse);
+    invokeRequest.SetLogType(Aws::Lambda::Model::LogType::Tail);
+    // Add dummy args to work around https://github.com/p-ranav/alpaca/issues/22#issuecomment-1569568081 
+    Detail::ArgsHolder argsHolder(std::tuple(0, 0, args...));
+    std::vector<uint8_t> bytes;
+    auto bytes_written = alpaca::serialize(argsHolder, bytes);
+    static_assert(sizeof(uint8_t) == sizeof(unsigned char), "Platform breaks our assumption that characters are 8 bits");
+    Aws::Utils::ByteBuffer byteBuffer(reinterpret_cast<unsigned char *>(&bytes.front()), bytes_written);
+    Aws::Utils::Json::JsonValue jsonPayload;
+    jsonPayload.WithString("serialized",  Aws::Utils::HashingUtils::Base64Encode(byteBuffer));
+    std::shared_ptr <Aws::IOStream> payload
+        = Aws::MakeShared<Aws::StringStream>("lambda argument", jsonPayload.View().WriteReadable());
+    invokeRequest.SetBody(payload);
+    invokeRequest.SetContentType("application/json");
+    client.client->InvokeAsync(
+      invokeRequest, 
+      [&](const Aws::Lambda::LambdaClient*, const Aws::Lambda::Model::InvokeRequest&, 
+          Aws::Lambda::Model::InvokeOutcome outcome, 
+          const std::shared_ptr<const Aws::Client::AsyncCallerContext>&) {
+        c(outcomeToExpected(outcome));
+      });
+  }
+
   EnhancedLambdaClient &client;
   std::string name;
 };
--- test/test_lambda_add.cpp
+++ test/test_lambda_add.cpp
@@ -7,7 +7,8 @@
 #include "awslabs/enhanced/lambda_client.h"
 #include "awslabs/enhanced/Aws.h"
 #include "gtest/gtest.h"
-
+#include <future>
+using namespace expns;
 namespace LambdaDecls {
     int add(int, int);
 }
@@ -21,4 +22,20 @@ protected:
 TEST_F(lambdaIntegrationTest, TestCall) {
     auto add = BIND_AWS_LAMBDA(client, LambdaDecls::add, "test_lambda_add_fn");
     EXPECT_EQ(add(1, 3), 4);
+}
+
+TEST_F(lambdaIntegrationTest, TestAsyncCall) {
+    auto add = BIND_AWS_LAMBDA(client, LambdaDecls::add, "test_lambda_add_fn");
+    std::promise<expected<int, std::string>> p;
+    auto f = p.get_future();
+    add.invoke_async([&p](expected<int, std::string> e){ 
+      try {
+        p.set_value(e.value());
+      } catch (...) {
+        try {
+          p.set_exception(std::current_exception());
+        } catch(...) {}
+      }
+    }, 2, 3);
+    EXPECT_EQ(f.get(), 5);
 }
