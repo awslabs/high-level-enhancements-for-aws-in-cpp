cb0731423c96a330aa6b76d1e01b82685daed121
Mike Spertus <mike@spertus.com>
2023-06-10 21:51:36 +0300

    sbiscigli's fix for macos build

--- include/awslabs/enhanced/detail/lambda_detail.h
+++ include/awslabs/enhanced/detail/lambda_detail.h
@@ -4,11 +4,17 @@ namespace AwsLabs::Enhanced::Detail {
 template<typename ...Ts>
 struct ArgsHolder {
    std::tuple<Ts...> tup;
+
+    ArgsHolder() = default;
+    explicit ArgsHolder(const std::tuple<Ts...> &tup) : tup(tup) {}
 };
 
 template<typename T>
 struct ResultHolder {
    T result;
+
+    ResultHolder() = default;
+    explicit ResultHolder(T const &result) : result(result) {}
 };
 
 }
